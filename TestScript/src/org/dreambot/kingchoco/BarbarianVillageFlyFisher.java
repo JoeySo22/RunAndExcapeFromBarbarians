aa14b69343e69d2561427754275d73510777550c
kingchocomilk <gamereg911@gmail.com>
2018-11-11 17:58:46 -0800

    More methods for FlyFisher and some other changes to the .gitignore

--- TestScript/src/org/dreambot/kingchoco/BarbarianVillageFlyFisher.java
+++ TestScript/src/org/dreambot/kingchoco/BarbarianVillageFlyFisher.java
@@ -1,5 +1,6 @@
 package org.dreambot.kingchoco;
 
+import org.dreambot.api.methods.container.impl.Inventory;
 import org.dreambot.api.methods.container.impl.bank.Bank;
 import org.dreambot.api.methods.map.Area;
 import org.dreambot.api.methods.map.Tile;
@@ -13,19 +14,23 @@ import org.dreambot.api.wrappers.interactive.Player;
         "and banks them", category = Category.FISHING, author = "KingChoco")
 public class BarbarianVillageFlyFisher extends AbstractScript
 {
-    private final Area BARBARIAN_FISHING_AREA = new Area(new Tile(), new Tile());
-    private final Area EDGEVILLE_BANKING_AREA = new Area(new Tile(), new Tile());
+    private final Area BARBARIAN_FISHING_AREA = new Area(new Tile(3109, 3439), new Tile(3100, 3422));
+    private final Area EDGEVILLE_BANKING_AREA = new Area(new Tile(3094, 3493), new Tile(3091, 3488));
     private final int FLY_FISHING_ROD_ID = 309;
     private final int FEATHERS_ID = 814;
     private final int FISHING_ANIMATION_ID = 623;
-
-
+    private final int RAW_TROUT_ID = 335;
+    private final int RAW_SALMON_ID = 331;
+    private final int COOKED_TROUT_ID = 333;
+    private final int COOKED_SALMON_ID = 329;
 
     private enum PlayerState
     {
         WALK_TO_BANK, WALKING_TO_FISHING_SPOT, FISHING, BANKING;
     }
+
     private PlayerState currentState;
+
     private Walking walkingObject;
 
     private Player localPlayer;
@@ -33,13 +38,63 @@ public class BarbarianVillageFlyFisher extends AbstractScript
     private Bank edgevilleBank;
 
     @Override
-    public void onStart() {
+    public void onStart()
+    {
+        log("onStart begun.");
         super.onStart();
+        this.walkingObject = getWalking();
+        this.localPlayer = getLocalPlayer();
+        this.
     }
 
     @Override
-    public int onLoop() {
-        
+    public int onLoop()
+    {
+        log("onLoop begins.");
+        currentState = checkPlayerState();
+        switch (currentState)
+        {
+            case BANKING:
+                banking();
+                break;
+            case FISHING:
+                fishing();
+                break;
+            case WALK_TO_BANK:
+                walkingToBank();
+                break;
+            case WALKING_TO_FISHING_SPOT:
+                walkingToFishingSpot();
+                break;
+        }
+        log("onLoop ends.");
         return 0;
+    }
+
+    private PlayerState checkPlayerState()
+    {
+        log("checkPlayerState open.");
+        Inventory inventory = getInventory();
+        Tile currentPlayerTile = getLocalPlayer().getTile();
+        boolean playerInFishingTile = BARBARIAN_FISHING_AREA.contains(currentPlayerTile);
+        boolean playerInBankingTile = EDGEVILLE_BANKING_AREA.contains(currentPlayerTile);
+        boolean playerHasFlyFishingRod = inventory.contains(FLY_FISHING_ROD_ID);
+        boolean playerHasFeathers = inventory.contains(FEATHERS_ID);
+        boolean playerHasFullInventory = inventory.isFull();
+
+        if (!playerHasFeathers || !playerHasFlyFishingRod)
+        {
+            return PlayerState.WALK_TO_BANK;
+        }
+        else if (playerHasFeathers && playerHasFlyFishingRod && playerHasFullInventory)
+        {
+            return PlayerState.WALK_TO_BANK;
+        }
+        else if (playerHasFeathers && playerHasFlyFishingRod && !playerHasFullInventory)
+        {
+            return PlayerState.WALKING_TO_FISHING_SPOT;
+        }
+        return PlayerState.WALK_TO_BANK;
+        log("checkPlayerState exit.");
     }
 }
